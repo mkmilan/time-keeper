cd9d50c0af5d4802e5e0648891a8e353b1f52fe7
Milan Margetic <13180642+mkmilan@users.noreply.github.com>
2026-01-18 17:07:39 +0100

    env plus mongo atlas

--- src/db.js
+++ src/db.js
@@ -1,6 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/equans_timesheet";
+dotenv.config();
+
+const env = process.env.NODE_ENV || "development";
+const MONGODB_URI =
+  (env === "production" ? process.env.MONGODB_URI_PROD : process.env.MONGODB_URI_DEV) ||
+  process.env.MONGODB_URI ||
+  "mongodb://127.0.0.1:27017/equans_timesheet";
 
 export async function connectDb() {
   mongoose.set("strictQuery", true);
